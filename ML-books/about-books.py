93fe9f571588add4762e42bcd52a1baf5ad3f67c
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-11-28 08:33:23 +0800

    Update about-books.py
    Add six books statement.

--- ML-books/about-books.py
+++ ML-books/about-books.py
@@ -1,1 +1,7 @@
 # These books are about machine learning.
+# 1.Introduction to Machine Learning with Python
+# 2.Python Machine Learning Cookbook
+# 3.Python project development combat(Japan BePROUD Co., Ltd.)
+# 4.Machine Learning and Data Science : An Introduction to Statistical Learning Methods with R
+# 5.Hands-On Mahine Learning with Scikit-Learn & TensorFlow
+# 6.Real-World Machine Learning
