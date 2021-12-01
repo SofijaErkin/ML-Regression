26505bebe72755e63cb9445cf633758e83522cad
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-02 07:30:53 +0800

    about-books.py
    Add a needed recommendation to recommend ebooks and the way to download them.

--- ML-books/about-books.py
+++ ML-books/about-books.py
@@ -8,3 +8,9 @@
 
 # Two books do not upload beacuse them are large than 30MB.
 # Please search those no-upload books if you need.
+
+# The better way to get the ebook is search book name add "PDF" or "Github"
+# Why for ebook, as i see it, ebook less weight, removeable, always stay with you or pc via the internet
+# Also, You cannot read a book well unless it is borrowed from others. 
+# As far as I'm concrened, ebook is borrowed from others.
+# Finally, having a paper book is the best.
