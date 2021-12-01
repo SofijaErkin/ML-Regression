b525a284b9ea3915756873368819b58f05f05838
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-01 09:03:24 +0800

    Update about-books.py
    Add a set of statements about being fault to uploading because that size is more than 30MB.

--- ML-books/about-books.py
+++ ML-books/about-books.py
@@ -1,7 +1,10 @@
 # These books are about machine learning.
-# 1.Introduction to Machine Learning with Python
+# 1.Introduction to Machine Learning with Python (Not upload)
 # 2.Python Machine Learning Cookbook
 # 3.Python project development combat(Japan BePROUD Co., Ltd.)
 # 4.Machine Learning and Data Science : An Introduction to Statistical Learning Methods with R
 # 5.Hands-On Mahine Learning with Scikit-Learn & TensorFlow
-# 6.Real-World Machine Learning
+# 6.Real-World Machine Learning (Not upload)
+
+# Two books do not upload beacuse them are large than 30MB.
+# Please search those no-upload books if you need.
