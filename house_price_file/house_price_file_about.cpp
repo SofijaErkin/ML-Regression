9b513440719daf8f9942859f16c129e20fcccde1
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-06 08:34:13 +0800

    Create house_price_file_about.cpp
    Add a statement about the house price files.

---
+++ house_price_file/house_price_file_about.cpp
@@ -0,0 +1,7 @@
+# This house price file includes six files.
+# 1.data_description.txt;
+# 2.oxzc_Data_Feature_Regression_NoPca_test_DeML.ipynb
+# 3.sample_submission.csv
+# 4.test.csv
+# 5.train.csv
+# 6.my_best.csv
